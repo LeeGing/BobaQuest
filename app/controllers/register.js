3fb8ec1d6d451cb0373afa89223b715321866f27
Edmond Lee <e.lee@live.ca>
2017-09-11 22:50:24 +0000

    registration.hbs edited

--- app/controllers/register.js
+++ app/controllers/register.js
@@ -3,20 +3,35 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
 
   actions: {
-    save(user) {
-      let newUser = user;
-      newUser.save().catch((error) => {
-          this.set('errorMessage', error)
-        })
-        .then(() => {
-          this.get('session')
-            .authenticate('authenticator:jwt',
-              newUser.get('email'), newUser.get('password'))
-            .catch((reason) => {
-              this.set('errorMessage', reason.error || reason);
-            });
-            this.transitionToRoute("index")
-        })
-    }
-  }
+    createUser: function() {
+      var email = this.get('userEmail');
+      var password = this.get('userPassword');
+       
+      var user = this.store.createRecord('user', {
+        email: email,
+        password: password
+      });
+
+      user.save();
+     }
+   }
 });
+  // actions: {
+  //   save(user) {
+  //     let newUser = user;
+  //     newUser.save().catch((error) => {
+  //         this.set('errorMessage', error)
+  //       })
+  //       .then(() => {
+  //         this.get('session')
+  //           .authenticate('authenticator:jwt',
+  //             newUser.get('email'), newUser.get('password'))
+  //           .catch((reason) => {
+  //             this.set('errorMessage', reason.error || reason);
+  //           });
+  //           this.transitionToRoute("index")
+  //       })
+  //   }
+  // }});
+// 
+
--- app/routes/register.js
+++ app/routes/register.js
@@ -1,30 +1,27 @@
 import Ember from 'ember';
 
 
-// export default Ember.Route.extend({
-//   model() {
-//     return this.store.createRecord('user')
-//   }
-// });
-
-
-
-
 export default Ember.Route.extend({
-
-	model: function(params) {
-
-		return Ember.$.ajax({
-
-  	url: 'http://localhost:3000/users/',
- 	  type: "POST",
- 	  data: JSON.stringify({
-
-    	email: "ab@ab.com" ,
-    	password: "1111"
-
-  	})
-
- 	})
-	}
+  model() {
+    return this.store.createRecord('user')
+  }
 });
+
+
+
+
+// export default Ember.Route.extend({
+
+// 	model: function(params) {
+
+// 		return Ember.$.ajax({
+
+//   	url: 'http://localhost:3000/users/',
+//  	  type: "POST",
+//  	  data: JSON.stringify({
+//     	email:  ,
+//     	password: 
+//   	})
+//  	})
+// 	}
+// });
