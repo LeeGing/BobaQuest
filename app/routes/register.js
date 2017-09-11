ae3d74c7547bac5bfe48e9e2039fb57c7e8fa3b2
Edmond Lee <e.lee@live.ca>
2017-09-11 15:44:59 +0000

    Setting up register

--- app/routes/register.js
+++ app/routes/register.js
@@ -1,8 +1,30 @@
 import Ember from 'ember';
 
 
+// export default Ember.Route.extend({
+//   model() {
+//     return this.store.createRecord('user')
+//   }
+// });
+
+
+
+
 export default Ember.Route.extend({
-  model() {
-    return this.store.createRecord('user')
-  }
+
+	model: function(params) {
+
+		return Ember.$.ajax({
+
+  	url: 'http://localhost:3000/users/',
+ 	  type: "POST",
+ 	  data: JSON.stringify({
+
+    	email: "ab@ab.com" ,
+    	password: "1111"
+
+  	})
+
+ 	})
+	}
 });
