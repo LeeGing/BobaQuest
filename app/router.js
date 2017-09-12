09f4e200b05afa4d786bff49a2c7c72348d97e42
Ricky <Na_ricky@hotmail.com>
2017-09-12 23:29:00 +0000

    Commit latest userpage 4:46PM

--- app/router.js
+++ app/router.js
@@ -8,8 +8,7 @@ const Router = Ember.Router.extend({
 
 Router.map(function() {
   this.route('users', { path: '/users/:id'}, function() {
-     // this.route('stores', { path: '/stores'});
-     // this.route('transactions', {path: '/:id/transactions'});
+     this.route('points')
   });
 
   this.route('stores', { path: '/stores/:id'});
