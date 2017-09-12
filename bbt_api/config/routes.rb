f36c415feeef13ac213883e77b8c8c53500b75f9
Ricky <Na_ricky@hotmail.com>
2017-09-12 23:26:19 +0000

    Userpage loads points

--- bbt_api/config/routes.rb
+++ bbt_api/config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
 
-  resources :users
+  resources :users do
+    resources :points
+  end
   resources :stores
+  resources :transactions
 
   post '/token', to: 'login#token'
+  # get '/users/:id/points' to: 'transactions#achievement'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
