957e76f6cf7433dcd78a90f470489bfed5d9d5f5
Edmond Lee <e.lee@live.ca>
2017-09-13 21:50:31 +0000

    Achievement helper function implemented

--- app/helpers/achievements.js
+++ app/helpers/achievements.js
@@ -2,10 +2,22 @@ import Ember from 'ember';
 
 export function achievements(params) {
 	let points = params
-	if (points > 25) {
-		return "Unlocked"
+	if (points >49) {
+		return "50+ Points!"
 	}
-	else if (points > 10) {
+	else if (points > 39) {
+		return "Level 4 (40Points)"
+	}
+	else if (points > 29) {
+		return "Level 3 (30Points)!"
+	}
+	else if (points > 19) {
+		return "Level 2 (20Points)!"
+	}
+	else if (points > 9) {
+		return "Level 1 (10Points)"
+	}
+	else {
 		return "Locked"
 	}
 }
