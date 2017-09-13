a7c3c028a9d0010f99613ea38cb063dad0a768ea
Ricky <Na_ricky@hotmail.com>
2017-09-13 20:25:27 +0000

    conditionalLogin made

--- app/routes/application.js
+++ app/routes/application.js
@@ -2,3 +2,30 @@ import Ember from 'ember';
 import ApplicationRouteMixin from 'ember-simple-auth/mixins/application-route-mixin';
 
 export default Ember.Route.extend(ApplicationRouteMixin);
+
+Ember.Route.extend({              //Natalie, I don't think it's reading this route.extend at all.
+
+  model: function(params) {
+    console.log('model params:', params);
+
+    return new Ember.RSVP.hash({
+      users: Ember.$.ajax({
+        type: 'GET',
+        url: `${config.apiHost}/users/${params.id}`
+      }),
+      points: Ember.$.ajax({
+        type: 'GET',
+        url: `${config.apiHost}/users/${params.id}/points`
+      })
+    })
+  },
+
+
+  setupController: function(controller, models) {
+
+    controller.set('user', models.users)
+    controller.set('points', models.points)
+
+  }
+
+});
