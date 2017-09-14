77fb2bd5551f447c00063cdc6df5491bdbc2a3f6
Edmond Lee <e.lee@live.ca>
2017-09-13 21:32:04 -0700

    achievement rendering foundations complete

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -3,6 +3,18 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   users: null,
 
+  isLvl1: Ember.computed('points', function (){
+   return this.get('points') > 9;
+	}), //conditional statement for achievements hbs
+
+	isLvl2: Ember.computed('points', function (){
+   return this.get('points') > 19;
+	}), //conditional statement for achievements hbs
+
+	isLvl3: Ember.computed('points', function (){
+   return this.get('points') > 29;
+	}), //conditional statement for achievements hbs
+
   actions: {
     signup: function() {
       console.log(this.get('userEmail'));
---
+++ app/helpers/achievements.js
@@ -0,0 +1,34 @@
+import Ember from 'ember';
+
+// POSSIBLY DELETE!!!!!!!
+
+// export function achievements(params) {
+// 	let points = params
+// 	if (points >49) {
+// 		return "50+ Points!"
+// 	}
+// 	else if (points > 39) {
+// 		return 'L1'
+
+// 	}
+// 	else if (points > 29) {
+// 		return "Level 3 (30Points)!"
+// 	}
+// 	else if (points > 19) {
+// 		return "Level 2 (20Points)!"
+// 	}
+// 	else if (points > 9) {
+// 		return "Level 1 (10Points)"
+// 	}
+// 	else {
+// 		return "Locked"
+// 	}
+// }
+
+// export function achievements(params) {
+// 	let points = params
+// 	if (points > 10) {
+// 		return true
+// 	}
+// }
+export default Ember.Helper.helper(achievements);
