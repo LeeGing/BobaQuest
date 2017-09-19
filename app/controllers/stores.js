171d9509789f4cf58ddb7e3e1d01eda237ab4e78
Edmond Lee <e.lee@live.ca>
2017-09-19 12:07:01 -0700

    stores css and hbs edited

--- app/controllers/stores.js
+++ app/controllers/stores.js
@@ -2,4 +2,8 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   stores: null,
+
+  sortedOrders: Ember.computed('order', function (){
+  	return this.get('order').reverse();
+  })
 });
